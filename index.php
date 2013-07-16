baec740b1065a8517b5dbb5d7bd024a059708acd
Mihai Maruseac <mihai@rosedu.org>
2013-07-16 14:23:23 +0300

    Remove 2011 source files (keep repo clean for 2013 edition).

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'smarty/Smarty.class.php';
-
-$smarty = new Smarty();
-$smarty->display('templates_c/index.tpl');
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-$IP = $_SERVER['REMOTE_ADDR'];
-session_start();
-if(isset($_SESSION['users'][$IP])) {
-	unset($_SESSION['users'][$IP]);
-	header('Location: /');
-}
-else
-	header('Location: /');
